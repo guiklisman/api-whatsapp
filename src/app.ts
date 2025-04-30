777c358eaaaf4862caf496457d789f12d3035533
Guilherme <guilhermeklisman@hotmail.com>
2025-04-30 18:52:37 -0300

    adicionado cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import apiRoutes from "./routes/api.ts";
 import whatsappService from "./services/whatsapp.services.ts";
 
 const app = express();
 
+app.use(cors()); // <- Aqui você habilita o CORS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
